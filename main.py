f9ba3c8748a8543533531676d80b5fe3ffb646bf
mkt <mkt@m-b-f.jp>
2018-04-18 19:10:04 +0900

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, Response
-from pyzbar import pyzbar
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-from datetime import datetime
-
-import numpy as np
-import cv2
-import time
